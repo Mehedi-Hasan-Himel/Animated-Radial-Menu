949bde6b5e86b2a6a9bf659fb2aab843474dd544
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-18 21:57:35 +0600

    code

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let toggle = document.querySelector(`.toggle`);
 let menu = document.querySelector(`.menu`);
-toggle.onclick = function() {
-   menu.classList.toggle(`active`)
-}
+
+toggle.onclick = function () {
+  menu.classList.toggle(`active`);
+};
+
+
